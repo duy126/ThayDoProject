d79192aada910a236a3d82d1923c59498301e63b
duy126 <passion393959@gmail.com>
2024-05-22 09:29:15 +0700

    new project

---
+++ dressingroom/dressingroom/models/ChoseItem.js
@@ -0,0 +1,28 @@
+const changerAo = (value)=>{
+ document.getElementById("ao").innerHTML = `<img src="../assets/images/clothes/topcloth${value}.png" alt="" />`
+
+}
+const changerQuan = (value)=>{
+    document.getElementById("quan").innerHTML = `<img src="../assets/images/clothes/botcloth${value}.png" alt="" />`
+   }
+const changerGiayDep = (value)=>{
+    document.getElementById("giayDep").innerHTML = `<img src="../assets/images/shoes/shoes${value}.png" alt="" />`
+   
+}
+const changerTuiXach = (value)=>{
+    document.getElementById("tuiXach").innerHTML = `<img src="../assets/images/handbags/handbag${value}.png" alt="" />`
+   
+}
+const changerDayChuyen = (value)=>{
+    document.getElementById("dayChuyen").innerHTML = `<img src="../assets/images/necklaces/necklace${value}.png" alt="" />`
+   
+}
+const changerKieuToc = (value)=>{
+    document.getElementById("kieuToc").innerHTML = `<img src="../assets/images/hairstyle/hairstyle${value}.png" alt="" />`
+   
+}
+const changerNen = (value)=>{
+    document.getElementById("nen").innerHTML = `<img src="../assets/images/background/background${value}.jpg" alt="" />`
+   
+}
+
